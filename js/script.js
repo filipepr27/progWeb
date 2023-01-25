066682f1b0fe1b69464dd039825b65a0f12f8633
Filipe Pádua <111016996+filipepr27@users.noreply.github.com>
2023-01-25 20:48:50 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let email = null
+let enviar
+
+function clickMenu() {
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+    }
+}
+
+function enviarMensagem() {
+    window.alert("Mensagem enviada!")
+}
+
+function cadastra() {
+    window.alert("Cadastro realizado!")
+}
+
